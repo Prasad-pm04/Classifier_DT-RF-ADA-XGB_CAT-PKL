1390e976a886bb9ad813362ec48231acec024b94
Prasad-pm04 <prasadmangade79@gmail.com>
2025-09-24 20:20:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout="wide"
 )
 
-st.image("web programmer logo.jpg")
+st.image("web_programmer_logo_cropped.jpg")
 
 # -------------------------
 # Load Dataset and Model
